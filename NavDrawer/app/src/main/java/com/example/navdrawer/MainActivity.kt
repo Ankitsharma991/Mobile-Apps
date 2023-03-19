70bda77a8a21f4eb5b3dbc3f4e5ea253d9e59c7f
Ankit Sharma <sharmaa7861@gmail.com>
2023-03-19 18:17:02 +0530

    completed Nav Drawer

--- NavDrawer/app/src/main/java/com/example/navdrawer/MainActivity.kt
+++ NavDrawer/app/src/main/java/com/example/navdrawer/MainActivity.kt
@@ -26,10 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -37,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_profile, R.id.nav_setting, R.id.nav_logout
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
